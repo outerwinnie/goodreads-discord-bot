02c56e44b7721196d18bc0685730ecac2668eff2
Winnie <outerwinnie@protonmail.com>
2023-07-25 15:14:27 +0200

    0.6

--- scrapper.py
+++ scrapper.py
@@ -1,13 +1,19 @@
 from typing import List
 import requests
+from bs4 import BeautifulSoup
+import rss_helper
 from rss_helper import RSSHelper, Book
 
-rss_feed = 'https://www.goodreads.com/user/updates_rss/35497141'
+rss_feed = rss_helper.rss_feed_url
 rsh = RSSHelper()
 books: List[Book] = rsh.get_rss_data(rss_feed)
 
 
 for i in books:
-        URL = books[i]["user_url"]
-        page = requests.get(URL)
-        print(page.text)
+    # URL = books[i]["user_url"]
+    URL = "https://www.goodreads.com/photo/user/50670314-winnie"
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+    result = soup.find(href="https://images.gr-assets.com/users/")
+    print(soup.text)
+
