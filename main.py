35ca3cabf475b32a7543a8ecaee65cc6a69fa1df
potajito <potajito@gmail.com>
2023-07-29 19:00:14 +0100

    logging test url parser test (broken, missing remove slah commands) updated gitinore

--- main.py
+++ main.py
@@ -4,19 +4,31 @@ from discord.ext import commands, tasks
 import datetime
 import logging
 import os
+from urllib.parse import urlparse
 from rich.logging import RichHandler
+from rich.traceback import install
 from typing import List
+from configuration import LOGLEVEL
 from rss_helper import RSSHelper, Review
 
+FORMAT = "%(message)s"
+logging.basicConfig(level=LOGLEVEL,
+                    format=FORMAT,
+                    datefmt="[%X]",
+                    handlers=[RichHandler(markup=True, rich_tracebacks=True)])
+log = logging.getLogger("rich")
 
+# Show Tracebacks if DEBUG
+if logging.root.level == logging.DEBUG:
+    install(show_locals=True)
+else:
+    install(show_locals=False)
+    
 time = datetime.datetime.now
-logging.basicConfig(level=logging.INFO)
 
 # Importing keys
 with open("data/config.txt", "r") as file:
     keys = [line for line in file]
-
-logging.debug("Arranca")
 
 try:
     DISCORD_TOKEN = os.environ['DISCORD_TOKEN']
@@ -27,7 +39,7 @@ except:
     GUILD_ID = keys[1]
     CHANNEL_ID = int(keys[2])
 
-logging.debug(f"Este es el ID del canal {CHANNEL_ID}")
+log.info(f":books: Starting Discord Bot on ChannelID {CHANNEL_ID} :books:")
 
 # Importing Users
 with open("data/users.txt", "r") as file:
@@ -55,12 +67,10 @@ class UpdatesClient(commands.Bot):
             await tree.sync(guild=discord.Object(
                 id=GUILD_ID))  # guild specific: leave blank if global (global registration can take 1-24 hours)
             self.synced = True
-            logging.debug("Conectado")
-        logging.debug("No Conectado")
-
+            
     @tasks.loop(seconds=5)
     async def timer(self, channel):
-        logging.debug("Starting timer")
+        log.debug(":stopwatch: Starting timer :stopwatch:")
         # if time().minute == 0 or time().minute == 30:
         if random.randrange(0, 2) == 1:
             if not self.msg_sent:
@@ -76,7 +86,7 @@ class UpdatesClient(commands.Bot):
                     embed.set_author(name=reviews[i]['username'],
                                      url=reviews[i]["user_url"], icon_url=reviews[i]["user_image_url"])
                     embed.set_thumbnail(url=reviews[i]['image_url'])
-                    logging.debug(f"Review enviada de {reviews[i]['username']}")
+                    log.debug(f"Review sent for user: {reviews[i]['username']}")
                     await channel.send(embed=embed, mention_author=True)
                 self.msg_sent = True
         else:
@@ -87,17 +97,19 @@ client = UpdatesClient(command_prefix='!', intents=discord.Intents().all())
 channel = client.get_channel(CHANNEL_ID)
 tree = client.tree
 
-
+    
 # Discord Slash Commands
 @tree.command(guild=discord.Object(id=GUILD_ID), name='add', description='Add User')  # guild specific
-async def add_user(interaction: discord.Interaction, user_id_input: int):
+async def add_user(interaction: discord.Interaction, user_input_url: str):
     await interaction.response.send_message("¡Añadido!", ephemeral=True)
     global users
     global reviews
 
+    user_id = extract_user_id_from_url(user_input_url)
+        
     with open("data/users.txt", "a") as file:
-        if user_id_input not in users:
-            file.writelines("\n" + str(user_id_input))
+        if user_id not in users:
+            file.writelines("\n" + str(user_id))
 
     with open("data/users.txt", "r") as file:
         users = [int(line) for line in file]
@@ -121,6 +133,17 @@ async def remove_user(interaction: discord.Interaction, user_id_input: int):
                 file.write(str(user))
         print(users)
         reviews = rsh.get_rss_data(users)
+
+def extract_user_id_from_url(url):
+    parsed_url = urlparse(url)
+    path_parts = parsed_url.path.split('/')
+
+    for part in path_parts:
+        if '.' in part or '-' in part:
+            user_id = part.split('.')[0].split('-')[0]
+            return user_id
+
+    return None  
 
 
 # Update Reviews
