b182e5144d37014cd8636c17821beefd36b4bedf
potajito <potajito@gmail.com>
2023-07-30 22:18:36 +0100

    fixes for scraping

--- bookwyrm.py
+++ bookwyrm.py
@@ -1,11 +1,12 @@
-
-import logging
+import traceback
+import logging, os
 from types import NoneType
 from typing import List
 from rich.logging import RichHandler
 from rich import print
 from rich.pretty import pprint
 from rich.traceback import install
+from rich.console import Console
 from bs4 import BeautifulSoup, NavigableString
 import re
 import requests
@@ -18,6 +19,7 @@ if logging.root.level == logging.DEBUG:
     install(show_locals=True)
 else:
     install(show_locals=False)
+console = Console()
 
 FORMAT = "%(message)s"
 logging.basicConfig(level=LOGLEVEL,
@@ -27,7 +29,7 @@ logging.basicConfig(level=LOGLEVEL,
 log = logging.getLogger("rich")
 
 rss_url = "https://bookwyrm.social/user/potajito/rss-reviews"
-user_profile_url = "https://bookwyrm.social/user/christa/"
+user_profile_url = "https://bookwyrm.social/user/potajito"
 
 def parse_book_name (s: str) -> str:
     """Extracts book name from a string, searching for the text between double quotes ("text")
@@ -70,7 +72,14 @@ def parse_score (s: str) -> int:
         log.debug(f"No score found on {s}")
         return 0
 
-# Find all 'a' tags with 'href' containing '/book/' and have "rated" under the <h3> tag with class "has-text-weight-bold"
+def append_to_url(url: str, path_to_append: str) -> str:
+    # Ensure path_to_append starts with a slash and url doesn't have one
+    if not url.endswith('/'):
+        url = f"{url}/"
+    if path_to_append.startswith('/'):
+        path_to_append = path_to_append[1:]
+        return url + path_to_append
+
 def find_book_title(entry: NavigableString) -> str:
     try:
         tag: NavigableString = entry.find('a', href=lambda href: href and '/book/' in href)
@@ -90,14 +99,35 @@ def find_book_author(entry: NavigableString) -> str:
             return 'Unknown author'        
     except Exception:
         return 'Unknown author'
-    
+
+def fill_review (title: str, score: int, author: str,
+                url: str, image_url: str, user_url: str,
+                username: str, user_image_url: str) -> Review:
+    """Adds fields to Review class
+
+    Args:
+        review (Review): _description_
+    """
+    current_review = {
+            "title": title,
+            "score": score,
+            "author": author,
+            "url": url,
+            "image_url": image_url,
+            "user_url": user_url,
+            "username": username,
+            "user_image_url": user_image_url
+            }
+    # log.debug(f"Added review: {current_review}")
+    return current_review 
+   
 def parse_user_profile (profile_url: str) -> dict:
     reviews: List[Review] = []
     try:
         profile_url_domain = urlparse(profile_url).hostname
         profile_url_scheme = urlparse(profile_url).scheme
-        reviews_url = requests.get(urljoin(user_profile_url,'reviews-comments'))
-        soup = BeautifulSoup(reviews_url.text,"html.parser")
+        reviews_url = append_to_url(user_profile_url,'/reviews-comments')
+        soup = BeautifulSoup(requests.get(reviews_url).text,"html.parser")
         
         user_image_url = soup.find('img', class_=re.compile(r'avatar image*')).get('src')
          
@@ -112,7 +142,7 @@ def parse_user_profile (profile_url: str) -> dict:
                 score = int(re.findall(r'\d+', score_in_stars)[0])
 
                 section_tag = entry.find_next('section', class_='card-content')
-                author = find_book_author(entry)
+                author = find_book_author(section_tag)
                 section_a_tags = section_tag.find_all('a')
                 section_img_tag = section_tag.find("img", class_="book-cover")
                 try:
@@ -126,12 +156,13 @@ def parse_user_profile (profile_url: str) -> dict:
                         
                         # log.debug(book_url)
                         break
-                
+                reviews.append(fill_review(book_name, score, author,
+                                           book_url, image_url, profile_url,
+                                           username, user_image_url))
                 clean_string = f"{username} rated {book_name} by {author}: {score}"
-                # log.info(clean_string)
+                log.info(clean_string)
             if ' reviewed ' in entry.text:
                 username = entry.find('span', itemprop='name').text.strip()
-                # book_name = entry.find(find_book_title).text.split()
                 book_name = find_book_title(entry)
                 author = find_book_author(entry)
 
@@ -152,25 +183,19 @@ def parse_user_profile (profile_url: str) -> dict:
                         
                         # log.debug(book_url)
                         break
-                current_review = {
-                "title": book_name,
-                "score": score,
-                "author": author,
-                "url": book_url,
-                "image_url": image_url,
-                "user_url": profile_url,
-                "username": username,
-                "user_image_url": user_image_url
-                }
-                pprint(current_review)
-                reviews.append(current_review)
-        # pprint(reviews)
+                reviews.append(fill_review(book_name, score, author,
+                            book_url, image_url, profile_url,
+                            username, user_image_url))
+                clean_string = f"{username} reviewed {book_name} by {author}: {score}"
+                log.info(clean_string)
+        log.info(f"Found {len(reviews)} reviews")
+        log.debug(pprint(reviews))
         return reviews
     
                         
     except Exception as error:
-        print('Could not parse the xml: ', profile_url)
-        print(error)
+        print('Could not parse:', reviews_url)
+        console.print_exception()
 
 parse_user_profile(user_profile_url)
 # parse_rss(rss_url)
